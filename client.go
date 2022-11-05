61a4d846d2e026ad5e7e85e1d092599f07b7eefa
yuanshuai <yuanshuai@gyyx.cn>
2022-11-05 22:34:58 +0800

    解析命令行

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -29,8 +30,19 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+var serverIp string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器IP地址（默认为127.0.0.1）")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口（默认是8888）")
+}
+
+
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+	// 命令行解析
+	flag.Parse()
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		fmt.Println(">>>>>>>>> 服务器链接失败")
 		return
