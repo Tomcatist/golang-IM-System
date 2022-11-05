59f10927e030f3ce5863d97eebd969132ec43e39
yuanshuai <yuanshuai@gyyx.cn>
2022-11-05 15:55:16 +0800

    用户业务封装

--- server.go
+++ server.go
@@ -54,13 +54,10 @@ func (this *Server) Broadcast(user *User, msg string) {
 func (this *Server) Handler(conn net.Conn) {
 	// ...当前链接的业务
 	//fmt.Println("链接建立成功")
-	user := NewUser(conn)
+	user := NewUser(conn, this)
+
 	// 用户上线，将用户加入到OnlineMap中
-	this.mapLock.Lock()
-	this.OnlineMap[user.Name] = user
-	this.mapLock.Unlock()
-	// 广播当前用户上线消息
-	this.Broadcast(user, "已上线")
+	user.Online()
 
 	// 接收客户端发送的消息
 	go func() {
@@ -68,7 +65,7 @@ func (this *Server) Handler(conn net.Conn) {
 		for {
 			n, err := conn.Read(buf)
 			if n == 0 {
-				this.Broadcast(user, "下线")
+				user.Offline()
 				return
 			}
 
@@ -79,8 +76,9 @@ func (this *Server) Handler(conn net.Conn) {
 
 			// 提取用户的消息
 			msg := string(buf[:n-1])
-			// 将得到的消息进行广播
-			this.Broadcast(user, msg)
+
+			// 用户针对消息进行处理
+			user.DoMessage(msg)
 		}
 	}()
 
